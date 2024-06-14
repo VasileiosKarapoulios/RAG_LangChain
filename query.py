d3e5ed487c774e5319833af2e83689b0e1799528
VasileiosKarapoulios <billkarap123@gmail.com>
2024-06-14 17:29:46 +0200

    comments

--- query.py
+++ query.py
@@ -27,23 +27,23 @@ Answer the question based on the above context: {question}
 
 def main():
 
-    # Prompt the user to enter the query text
+    # Prompt to enter the query text
     query_text = input("Enter query: ")
 
-    # Prepare the DB.
+    # Prepare the DB
     embedding_function = OpenAIEmbeddings()
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
     # Initialize model
     model = ChatOpenAI()
 
-    # Get the model's answer without context.
+    # Get the model's answer without context
     direct_response_text = model.predict(query_text)
     print(f"Direct response: {direct_response_text}")
 
     print("\n\nLooking at the database...\n\n")
 
-    # Search the DB.
+    # Search the DB
     results = db.similarity_search_with_relevance_scores(query_text, k=20)
     if len(results) == 0 or results[0][1] < 0.7:
         print(f"Unable to find matching results.")
@@ -52,7 +52,6 @@ def main():
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
-    # print(prompt)
 
     response_text = model.predict(prompt)
 
